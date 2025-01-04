5dbc91b8e44188aabdb6068c43356a768ba5ec2c
Marko <49600278+clearfeld@users.noreply.github.com>
2025-01-03 21:03:44 -0500

    Fix: sidebar drag vs content width calucation

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { Routes, Route, Outlet } from "react-router";
 
 import "./updater.tsx";
 import { H5 } from "@controlkit/ui";
+import TabBar from "./commons/tabbar/index.tsx";
 
 const styles = stylex.create({
 	container: {
@@ -28,14 +29,13 @@ const styles = stylex.create({
 	},
 
 	content_width: {
-		// width: "calc(100% - var(--sidebar-width))",
-		width: "calc(100%)",
+		width: "calc(100% - var(--sidebar-width))",
 	},
 
 	details_container: {
 		display: "flex",
 		width: "100%",
-		// height: "calc(100% - 2rem)", // tabbar-height
+		// height: "calc(100% - var(--tabbar-height))",
 		height: "100%",
 	},
 
